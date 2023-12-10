0f54e8dd52a72775e30ec85f41edeb28f9df36eb
Ashraf Beshtawi <ashraf.beshtawi@foo.ag>
2023-12-10 17:45:18 +0100

    adding autoloader

---
+++ php/html/index.php
@@ -0,0 +1,4 @@
+<?php
+namespace main;
+require_once __DIR__ . '/../vendor/autoload.php';
+
---
+++ php/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'a9532854ab8783340d97a5d9bf478400ea723b3b',
+        'name' => 'your-vendor-name/your-project-name',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'your-vendor-name/your-project-name' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'a9532854ab8783340d97a5d9bf478400ea723b3b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
